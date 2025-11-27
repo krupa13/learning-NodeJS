eddbfbf257a325c8018fea07f894a4ceb68da974
Krupa Nandh <kruparaomui@gmail.com>
2025-11-27 12:52:27 +0530

    Middleware

--- src/app.js
+++ src/app.js
@@ -2,19 +2,42 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    //Save data to DB
-    res.send({firstName: "Krupa", lastName: "Nandh"});
-});
+// app.get("/user/:userId/:name/:password", (req, res) => {
+//     console.log(req.params);
+//     //Save data to DB
+//     res.send({firstName: "Krupa", lastName: "Nandh"});
+// });
 
-app.post("/user", (req, res) => {
-    res.send("Post data saved successfully.");
-});
+// app.post("/user", (req, res) => {
+//     res.send("Post data saved successfully.");
+// });
 
-app.delete("/user", (req, res) => {
-    res.send("Data deleted successfully.");
-});
+// app.delete("/user", (req, res) => {
+//     res.send("Data deleted successfully.");
+// });
+
+app.use(
+    "/user", 
+    [(req, res, next) => {
+        console.log("Handling user request 1");
+        //res.send("Response 1");
+        next();
+    },
+    (req, res, next) => {
+        console.log("Handling request 2");
+        //res.send("Response 2");
+        next();
+    }],
+    (req, res, next) => {
+        console.log("Handling request 3");
+        //res.send("Response 3");
+        next();
+    },
+    (req, res) => {
+        console.log("Handling request 4");
+        res.send("Response 4");
+    }
+);
 
 app.listen(3000, () => {
     console.log("server successfull");
