cae1b15ad82396d251d388338b090a91cad0bdd0
Krupa Nandh <kruparaomui@gmail.com>
2025-11-29 20:20:12 +0530

    saved data using middleware

--- src/app.js
+++ src/app.js
@@ -1,33 +1,31 @@
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-    //Creating new instance of the user model
-    const user = new User({
-        firstName: "Abhishek",
-        lastName: "Mudimanchi",
-        emailId: "abhi@gmail.com",
-        password: "abhi@123",
-    });
+    const user = new User(req.body);
 
     try{
       await user.save();
       res.send("User Added Successfully");
     } catch(err) {
       res.status(400).send("Error in adding user: " + err.message);
-    } 
-
+    }
 });
 
+console.log("Attempting to connect to MongoDB...");
 connectDB()
     .then(() => {
-        console.log("Database connection");
+        console.log("Database connection successful");
         app.listen(7777, () => {
             console.log("Server is successfully connected...");
         });
     })
     .catch((err) => {
-        console.error("Database cannot be connected");
+        console.error("Database connection failed:", err.message);
+        process.exit(1);
     });
--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    await mongoose.connect(
-        "mongodb+srv://krupamr2795_db_user:nSrAokeXu1VnG8kh@krupadev.ux082xp.mongodb.net/devTinder"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
 };
 
 module.exports = connectDB;
