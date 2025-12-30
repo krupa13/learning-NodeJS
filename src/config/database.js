91e7c285d095334bd8dc2587af6da2df5742e019
Krupa Nandh <kruparaomui@gmail.com>
2025-12-30 18:24:33 +0530

    updated database js file

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI);
 };
 
