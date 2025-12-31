9763e0d0ad1e912c3f5dafa9f55223525f8b9beb
Krupa Nandh <kruparaomui@gmail.com>
2025-12-31 17:08:46 +0530

    changes in dotenv

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-
-require("dotenv").config();
 
 const corsOptions = {
   origin: "http://localhost:5173",
