ae69f64354ee0906e3f81d0ec44bd33c05f37ab5
Krupa Nandh <kruparaomui@gmail.com>
2026-01-06 23:13:38 +0530

    updated payment details

--- src/models/payment.js
+++ src/models/payment.js
@@ -1,46 +1,48 @@
 const mongoose = require("mongoose");
 
-const paymentSchema = new mongoose.Schema({
+const paymentSchema = new mongoose.Schema(
+  {
     userId: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     paymentId: {
-        type: String,
+      type: String,
     },
     orderId: {
-        type: String,
-        required: true
-    },
-    amount: {
-        type: Number,
-        required: true,
+      type: String,
+      required: true,
     },
     status: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
     },
     currency: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     receipt: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     notes: {
-        firstName: {
-            type: String,
-        },
-        lastName: {
-            type: String,
-        },
-        membershipType: {
-            type: String
-        }
-    }
+      firstName: {
+        type: String,
+      },
+      lastName: {
+        type: String,
+      },
+      membershipType: {
+        type: String,
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-}, { timestamps: true });
-
-module.exports = mongoose.model('payment', paymentSchema);
+module.exports = mongoose.model("Payment", paymentSchema);
